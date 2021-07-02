bfdd99af4e6d035e8d072f0707d7db02bc4d7067
Lloyd Barnes <barnes.lloyd86@gmail.com>
2021-07-02 03:20:44 -0500

    updated index.html

--- logic.js
+++ logic.js
@@ -33,6 +33,7 @@ let startYear = $("#start-year")
 
   if (parseInt(endYear)) {
     queryParams.end_date = endYear + "0101";
+  }
 
       // Logging the URL so we have access to it for troubleshooting
   console.log("---------------\nURL: " + queryURL + "\n---------------");
@@ -83,3 +84,65 @@ function updatePage(NYTData) {
            "</strong>"
        );
      }
+     // If the article has a byline, log and append to $articleList
+    let byline = article.byline;
+
+    if (byline && byline.original) {
+      console.log(byline.original);
+      $articleListItem.append("<h5>" + byline.original + "</h5>");
+    }
+
+    // Log section, and append to document if exists
+    let section = article.section_name;
+    console.log(article.section_name);
+    if (section) {
+      $articleListItem.append("<h5>Section: " + section + "</h5>");
+    }
+
+    // Log published date, and append to document if exists
+    let pubDate = article.pub_date;
+    console.log(article.pub_date);
+    if (pubDate) {
+      $articleListItem.append("<h5>" + article.pub_date + "</h5>");
+    }
+
+    // Append and log url
+    $articleListItem.append("<a href='" + article.web_url + "'>" + article.web_url + "</a>");
+    console.log(article.web_url);
+
+    // Append the article
+    $articleList.append($articleListItem);
+  }
+}
+
+// Function to empty out the articles
+function clear() {
+  $("#article-section").empty();
+}
+
+// CLICK HANDLERS
+// ==========================================================
+
+// .on("click") function associated with the Search Button
+$("#run-search").on("click", function(event) {
+  // This line allows us to take advantage of the HTML "submit" property
+  // This way we can hit enter on the keyboard and it registers the search
+  // (in addition to clicks). Prevents the page from reloading on form submit.
+  event.preventDefault();
+
+  // Empty the region associated with the articles
+  clear();
+
+  // Build the query URL for the ajax request to the NYT API
+  let queryURL = buildQueryURL();
+
+  // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+  // The data then gets passed as an argument to the updatePage function
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(updatePage);
+});
+
+//  .on("click") function associated with the clear button
+$("#clear-all").on("click", clear);
