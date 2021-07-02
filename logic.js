544ceb5c7ca60fb4e83f0e6b88e4cdcd72bd008e
Lloyd Barnes <barnes.lloyd86@gmail.com>
2021-07-02 03:01:44 -0500

    updated logic.js

--- logic.js
+++ logic.js
@@ -4,11 +4,11 @@
  */
  function buildQueryURL() {
     // queryURL is the url we'll use to query the API
-    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
   
     // Begin building an object to contain our API call's query parameters
     // Set the API key
-    var queryParams = { "api-key": "S7nxIhnReAWrgZzruqg4uDOZ4YItUUc4" };
+    let queryParams = { "api-key": "S7nxIhnReAWrgZzruqg4uDOZ4YItUUc4" };
 
     // Grab text the user typed into the search input, add to the queryParams object
   queryParams.q = $("#search-term")
@@ -16,9 +16,21 @@
   .trim();
 
 // If the user provides a startYear, include it in the queryParams object
-var startYear = $("#start-year")
+let startYear = $("#start-year")
   .val()
   .trim();
   if (parseInt(startYear)) {
     queryParams.begin_date = startYear + "0101";
+  }
+  if (parseInt(startYear)) {
+    queryParams.begin_date = startYear + "0101";
+  }
+
+  // If the user provides an endYear, include it in the queryParams object
+  let endYear = $("#end-year")
+    .val()
+    .trim();
+
+  if (parseInt(endYear)) {
+    queryParams.end_date = endYear + "0101";
   }
